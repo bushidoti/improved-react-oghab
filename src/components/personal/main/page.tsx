5df3befe09a9e4372233adaaffc8e731e44e70fd
Sajjad <titanxl79@gmail.com>
2023-09-29 12:07:00 +0330

    fixed columns and ad all columns of personal table complex search and sort date search left working on it

--- src/components/personal/main/page.tsx
+++ src/components/personal/main/page.tsx
@@ -2,7 +2,7 @@ import { SearchOutlined } from '@ant-design/icons';
 import React, {useEffect, useRef, useState} from 'react';
 import Highlighter from "react-highlight-words";
 import type {InputRef, TableProps} from 'antd';
-import { Button, Input, Space, Table } from 'antd';
+import {Badge, Button, Input, Space, Table} from 'antd';
 import type { ColumnType, ColumnsType } from 'antd/es/table';
 import type {FilterConfirmProps, FilterValue, SorterResult} from 'antd/es/table/interface';
 import Url from "../../api-configue";
@@ -14,6 +14,19 @@ interface DataType {
   full_name: string;
   job: string;
   office: string;
+  date: string;
+  sex: string;
+  national_id: string;
+  approvedPrice: string;
+  commitmentPrice: string;
+  typeBail: string;
+  firstBail: string;
+  secondBail: string;
+  expireDate: string;
+  clearedDate: string;
+  clearedStatus : boolean;
+  receivedDocument  : boolean;
+  affidavitStatus  : boolean;
 }
 
 type DataIndex = keyof DataType;
@@ -65,12 +78,60 @@ const MainPersonal: React.FC = () => {
     setSearchText('');
   };
 
+  const handleSearchPlaceHolder = (dataIndex: DataIndex) => {
+
+    if (dataIndex === "id") {
+      return 'کد ثبت'
+    }else if (dataIndex === "full_name") {
+      return 'نام و نشانی'
+    }else if (dataIndex === "national_id") {
+      return 'کد ملی'
+    }else if (dataIndex === "job") {
+      return 'شغل'
+    }
+
+  }
+
+  const handleTypeFirstBail = (type:string) => {
+
+    if (type === "چک") {
+      return 'شماره چک'
+    }else if (type === "نقد") {
+      return 'واریز به حساب'
+    }else if (type === "سفته") {
+      return 'تعداد سفته'
+    }else if (type === "بانک") {
+      return 'ضمانت'
+    }else if (type === "تعهد") {
+      return 'موضوع تعهد'
+    }
+
+  }
+
+
+  const handleTypeSecondBail = (type:string) => {
+
+    if (type === "چک") {
+      return 'بانک'
+    }else if (type === "نقد") {
+      return 'شماره حساب'
+    }else if (type === "سفته") {
+      return 'مبلغ سفته'
+    }else if (type === "بانک") {
+      return 'شماره تضمین'
+    }else if (type === "تعهد") {
+      return 'شماره تعهد'
+    }
+
+  }
+
   const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<DataType> => ({
+
     filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
       <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
         <Input
           ref={searchInput}
-          placeholder={`جستجو ${dataIndex}`}
+          placeholder={`جستجو ${handleSearchPlaceHolder(dataIndex)}`}
           value={selectedKeys[0]}
           onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
           onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
@@ -148,20 +209,22 @@ const MainPersonal: React.FC = () => {
       align:"center",
       title: 'شماره ثبت',
       dataIndex: 'id',
+      width: '4.88%',
+      fixed: 'left',
       key: 'id',
-      width: '15%',
       ...getColumnSearchProps('id'),
       sorter: (a, b) => a.id - b.id,
       sortOrder: sortedInfo.columnKey === 'id' ? sortedInfo.order : null,
       sortDirections: ['descend', 'ascend'],
       filteredValue: filteredInfo.id || null,
-      ellipsis: true,
+      
     },{
       align:"center",
       title: 'وضعیت',
+      fixed: 'left',
       dataIndex: 'type',
+      width: '5%',
       key: 'type',
-      width: '20%',
       filters: [
           {
             text: 'قراردادی',
@@ -173,31 +236,169 @@ const MainPersonal: React.FC = () => {
       ],
       filteredValue: filteredInfo.type || null,
       onFilter: (value, record) => record.type === value,
-      ellipsis: true,
+      
     },{
       align:"center",
       title: 'نام و نشانی',
+      fixed: 'left',
       dataIndex: 'full_name',
+      width: '5.88%',
       key: 'full_name',
-      width: '30%',
       ...getColumnSearchProps('full_name'),
       filteredValue: filteredInfo.full_name || null,
-      ellipsis: true,
+      
+    },{
+      align:"center",
+      title: 'جنسیت',
+      width: '4%',
+      dataIndex: 'sex',
+      key: 'sex',
+      filters: [
+          {
+            text: 'مذکر',
+            value: 'مذکر',
+          },{
+            text: 'مونث',
+            value: 'مونث',
+          }
+      ],
+      filteredValue: filteredInfo.sex || null,
+      onFilter: (value, record) => record.sex === value,
+      
+    },{
+      align:"center",
+      title: 'تاریخ استخدام',
+      dataIndex: 'date',
+      width: '5%',
+      key: 'date',
+      ...getColumnSearchProps('date'),
+      filteredValue: filteredInfo.date || null,
+      
+    },{
+      align:"center",
+      title: 'کد ملی',
+      width: '5.88%',
+      dataIndex: 'national_id',
+      key: 'national_id',
+      ...getColumnSearchProps('national_id'),
+      filteredValue: filteredInfo.national_id || null,
+      
     },{
       align:"center",
       title: 'شغل',
+      width: '5.88%',
       dataIndex: 'job',
       key: 'job',
-      width: '20%',
       ...getColumnSearchProps('job'),
       filteredValue: filteredInfo.job || null,
-      ellipsis: true,
-    },
-    {
+      
+    },{
+      align:"center",
+      title: 'تضمین مصوب',
+      dataIndex: 'approvedPrice',
+      width: '5.88%',
+      key: 'approvedPrice',
+    },{
+      align:"center",
+      title: 'مبلغ تضمین',
+      width: '5.88%',
+      dataIndex: 'commitmentPrice',
+      key: 'commitmentPrice',
+
+    },{
+      align:"center",
+      title: 'وثیقه تضمین',
+      width: '5%',
+      dataIndex: 'typeBail',
+      key: 'typeBail',
+      filters: [
+          {
+            text: 'چک',
+            value: 'چک',
+          },{
+            text: 'نقد',
+            value: 'نقد',
+          },{
+            text: 'سفته',
+            value: 'سفته',
+          },{
+            text: 'بانک',
+            value: 'بانک',
+          },{
+            text: 'تعهد',
+            value: 'تعهد',
+          }
+      ],
+      filteredValue: filteredInfo.typeBail || null,
+      onFilter: (value, record) => record.typeBail === value,
+    },{
+      align:"center",
+      title: 'مشخصه وثیقه',
+      children:[
+        {
+          dataIndex: 'firstBail',
+          key: 'firstBail',
+          align:"center",
+          width: '10%',
+          render: (value, record, index) => handleTypeFirstBail(record.typeBail) + ' : ' + record.firstBail ,
+
+        },{
+          dataIndex: 'secondBail',
+          key: 'secondBail',
+          align:"center",
+          width: '10%',
+          render: (value, record, index) => handleTypeSecondBail(record.typeBail) + ' : ' + record.secondBail ,
+        }
+      ],
+    },{
+      align:"center",
+      title: 'تاریخ پایان قرارداد',
+      dataIndex: 'expireDate',
+      width: '5%',
+      key: 'expireDate',
+      ...getColumnSearchProps('expireDate'),
+      filteredValue: filteredInfo.expireDate || null,
+
+    },{
+      align:"center",
+      title: 'وضعیت تسویه',
+      width: '5%',
+      dataIndex: 'clearedStatus',
+      key: 'clearedStatus',
+      render: (value, record, index) => record.clearedStatus ? <Badge status="success"/> : <Badge status="error"/> ,
+
+
+    },{
+      align:"center",
+      title: 'تاریخ تسویه',
+      width: '5%',
+      dataIndex: 'clearedDate',
+      key: 'clearedDate',
+      ...getColumnSearchProps('clearedDate'),
+      filteredValue: filteredInfo.expireDate || null,
+
+    },{
+      align:"center",
+      title: 'وضعیت مدرک',
+      width: '5%',
+      dataIndex: 'receivedDocument',
+      key: 'receivedDocument',
+      render: (value, record, index) => record.receivedDocument ? <Badge status="success"/> : <Badge status="error"/> ,
+
+    },{
+      align:"center",
+      title: 'وضعیت اقرارنامه',
+      width: '5%',
+      dataIndex: 'affidavitStatus',
+      key: 'affidavitStatus',
+      render: (value, record, index) => record.affidavitStatus ? <Badge status="success"/> : <Badge status="error"/> ,
+
+    },{
       align:"center",
       title: 'محل کار',
+      width: '5.88%',
+      fixed: 'right',
       dataIndex: 'office',
-      ellipsis: true,
       key: 'office',
       filters: [
       {
@@ -248,7 +449,7 @@ const MainPersonal: React.FC = () => {
             <Button onClick={clearFilters}>پاک کردن فیتلر ها</Button>
             <Button onClick={clearAll}>پاک کردن فیلتر و مرتب کننده ها</Button>
           </Space>
-          <Table bordered columns={columns} dataSource={contract} rowKey="id" onChange={handleChange}  loading={loading} pagination={{position:["bottomCenter"]}}/>;
+          <Table bordered columns={columns}  dataSource={contract} scroll={{ x: 3000 }} rowKey="id" onChange={handleChange}  loading={loading} pagination={{position:["bottomCenter"]}}/>;
       </>
   )
 };

ff1f87ae6ec58399acdb8db3100ef25a2b527564
Sajjad <titanxl79@gmail.com>
2024-02-02 22:32:41 +0330

    connect to date event api and display in banner

--- src/components/layout/banner.tsx
+++ src/components/layout/banner.tsx
@@ -8,14 +8,11 @@ import axios from "axios";
 
 export const Banner = () => {
     let today = new Date().toLocaleDateString('fa-IR', { year: 'numeric', month: 'long', day: 'numeric' });
-    let dayEvent = new Date().toLocaleDateString('fa-IR-u-nu-latn', {day: 'numeric' });
-    let monthEvent = new Date().toLocaleDateString('fa-IR-u-nu-latn', {month: 'numeric' });
-    let yearEvent = new Date().toLocaleDateString('fa-IR-u-nu-latn', {year: 'numeric' });
     const day = new Date().toLocaleString('fa-IR', {  weekday: 'long' })
     const context = useContext(Context)
     const [message , setMessage] = useState<any>('')
     const [event , setEvent] = useState<any>([])
-
+    console.log(event)
     useEffect(() => {
         if (context.isLogged) {
             (async () => {
@@ -28,12 +25,16 @@ export const Banner = () => {
                 setMessage(data[0].message);
             })().then(
                 async () => {
-                const {data} = (await axios.get(`https://holidayapi.ir/jalali/${yearEvent}/${monthEvent}/${dayEvent}`));
-                     setEvent(data.events)
+                const {data} = (await axios.get(`${Url}/date`, {
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                    }
+                }));
+                     setEvent(data.message.events)
             }
             )
         }
-    }, [context.isLogged, monthEvent, yearEvent]);
+    }, [context.isLogged]);
 
 
     return (

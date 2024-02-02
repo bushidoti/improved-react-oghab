0a2427b6c53d1bee32e4200266ed31c94c1c182e
Sajjad <titanxl79@gmail.com>
2024-02-02 21:24:10 +0330

    connect to date event api and display in banner

--- src/components/layout/banner.tsx
+++ src/components/layout/banner.tsx
@@ -8,10 +8,14 @@ import axios from "axios";
 
 export const Banner = () => {
     let today = new Date().toLocaleDateString('fa-IR', { year: 'numeric', month: 'long', day: 'numeric' });
+    let dayEvent = new Date().toLocaleDateString('fa-IR-u-nu-latn', {day: 'numeric' });
+    let monthEvent = new Date().toLocaleDateString('fa-IR-u-nu-latn', {month: 'numeric' });
+    let yearEvent = new Date().toLocaleDateString('fa-IR-u-nu-latn', {year: 'numeric' });
     const day = new Date().toLocaleString('fa-IR', {  weekday: 'long' })
     const context = useContext(Context)
-
     const [message , setMessage] = useState<any>('')
+    const [event , setEvent] = useState<any>([])
+
     useEffect(() => {
         if (context.isLogged) {
             (async () => {
@@ -22,19 +26,34 @@ export const Banner = () => {
                     }
                 }));
                 setMessage(data[0].message);
-            })()
+            })().then(
+                async () => {
+                const {data} = (await axios.get(`https://holidayapi.ir/jalali/${yearEvent}/${monthEvent}/${dayEvent}`));
+                     setEvent(data.events)
+            }
+            )
         }
-    }, [context.isLogged]);
+    }, [context.isLogged, dayEvent, monthEvent, yearEvent]);
+
 
     return (
-          <Marquee speed={50} className='bg-amber-100' pauseOnHover gradient={false}>
+          <Marquee speed={100} className='bg-amber-100' pauseOnHover gradient={false}>
               <Space size={500}>
-                  <p>
-                    {`امروز ${day} , ${today}`}
-                  </p>
-                  <p>
-                      {message}
-                  </p>
+                  {
+                      event.map((data: any , i : number) => (
+                          <p key={i}>
+                              {`رویداد امروز :  ${data?.description}`}
+                          </p>
+                      ))
+                  }
+                    {
+                      [`امروز ${day} , ${today}`,message].map((data: string) => (
+                          <p className={'me-4'} key={data}>
+                               {data}
+                          </p>
+                      ))
+                  }
+
               </Space>
           </Marquee>
     )
